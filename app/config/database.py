ba3e4df0bee526952960670ac636c0b4a0fc7d30
wesley1263 <weslei.andrade.souza@gmail.com>
2023-12-03 14:49:18 -0300

    Feature:  - Adjust config database to test

--- app/config/database.py
+++ app/config/database.py
@@ -7,7 +7,7 @@ settings = get_settings()
 
 """ This config is for generate migrations by aerich """
 TORTOISE_ORM = {
-    "connections": {"default": settings.DB_URL},
+    "connections": {"default": settings.DB_URL if not settings.TESTING else settings.DB_TEST_URL},
     "apps": {
         "models": {
             "models": settings.ENTITIES,
@@ -20,6 +20,9 @@ TORTOISE_ORM = {
 async def connect_to_database() -> None:
     logger.info("Initialize Tortoise...")
     await Tortoise.init(config=TORTOISE_ORM)
+    if settings.TESTING:
+        logger.info("Creating test database...")
+        await Tortoise.generate_schemas()
 
 
 async def close_connection_database() -> None:
